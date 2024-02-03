79ecc3a1676a396b613a44c89a84cea5feb571e8
alexandramastrangelo <arose62@comcast.net>
2024-02-03 01:13:04 -0500

    added home page and routes

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+// Example: src/pages/HomePage.js
+import React from "react";
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            {/* Add more content or components here */}
+        </div>
+    );
+}
+
+export default HomePage;
